488ac1fd3113cb3d922c99b39b89f47f041befc6
jasleenwahi <jasleen.wahi@knoldus.com>
2023-04-22 11:53:23 +0530

    my first in testing

---
+++ learntesting/src/main/java/com/example/learntesting/Student.java
@@ -0,0 +1,43 @@
+package com.example.learntesting;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+
+
+@ToString
+@Getter
+@Setter
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table
+public class Student {
+    @Id
+    @SequenceGenerator(
+            name = "student_sequence",
+            sequenceName = "student_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            generator = "student_sequence",
+            strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Gender gender;
+    int abc;
+    public Student(String name, String email, Gender gender) {
+        this.name = name;
+        this.email = email;
+        this.gender = gender;
+    }
+}
---
+++ learntesting/src/test/java/com/example/learntesting/LearntestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.learntesting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearntestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
